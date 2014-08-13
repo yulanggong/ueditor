9d12734661fd285b3e09f2cc7b0fbcd859edbdf5
yulanggong <yulanggong@gmail.com>
2014-08-13 09:25:34 +0800

    改变了移除草稿的实现方式

--- _examples/main.js
+++ _examples/main.js
@@ -13,7 +13,7 @@ requirejs.config({
 define(['jquery'], function ($) {
   var editor = UE.getEditor('ueditor');
   clearlocaldata = function(){
-    var setkey = ( location.protocol + location.host + location.pathname ).replace( /[.:\/]/g, '_' ) + 'ueditor-drafts-data';
+    var setkey = location.href.split(/[\?#]/)[0].replace('://', '_').replace( /[.:\/]/g, '_' ) + 'ueditor-drafts-data';
     if(setkey) {
       var root = JSON.parse(window.localStorage.ueditor_preference)
       delete root[setkey]
